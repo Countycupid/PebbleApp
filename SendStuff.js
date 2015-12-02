db471408e69efbce650233060ce8b865a60d1113
Countycupid <ketelerskevin@gmail.com>
2015-12-02 16:25:14 +0100

    Update SendStuff.js

--- SendStuff.js
+++ SendStuff.js
@@ -6,3 +6,16 @@ function ReturnStuff(){
     var return_to = getQueryParam('return_to', 'pebblejs://close#');
     document.location = return_to + encode(json.stringify(getAndStoreConfigData()));
 };
+
+function getQueryParam(variable, defaultValue) {
+    var query = location.search.substring(1);
+    var data = query.split('&');
+    for (var i = 0; i < data.length;i++){
+        var pair = data[i].spli('=');
+        if (pair[0] === variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    return defaultValue || false;
+}
+
